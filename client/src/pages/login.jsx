9daf5c25a47015716e10aeec2c27674bca99eed5
Sarthak Vats <sarthakvats121212@gmail.com>
2024-09-16 23:17:30 +0530

    server delay notification added

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { signup, signin } from "../api/pokedex.js";
 import { useNavigate } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress";
@@ -10,7 +10,14 @@ function Auth() {
   const [loading, setLoading] = useState(false); // Loading state
   const [signingError, setSigningError] = useState(false);
   const [errorValue, setErrorValue] = useState("");
+  const [showNotification, setShowNotification] = useState(true); // State to control notification visibility
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // Auto-hide the notification after a few seconds
+    const timer = setTimeout(() => setShowNotification(false), 10000);
+    return () => clearTimeout(timer);
+  }, []);
 
   async function handleSignup(e) {
     e.preventDefault();
@@ -38,7 +45,6 @@ function Auth() {
     } catch (error) {
       setSigningError(true);
       setErrorValue(error.response?.data?.message);
-
       console.log(`Error occurred during login: ${error}`);
     } finally {
       setLoading(false); // Stop loading after login completes
@@ -47,6 +53,20 @@ function Auth() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-blue-300">
+      {showNotification && (
+        <div className="fixed top-0 left-0 w-full bg-yellow-500 text-white p-4 text-center shadow-lg">
+          <p>
+            Since this website is deployed on a free service, it may take a few
+            minutes to start the backend server. Please be patient.
+          </p>
+          <button
+            onClick={() => setShowNotification(false)}
+            className="ml-4 bg-yellow-600 px-3 py-1 rounded text-white font-bold hover:bg-yellow-700"
+          >
+            Close
+          </button>
+        </div>
+      )}
       <div className="bg-blue-400 p-8 shadow-lg rounded-lg max-w-md w-full">
         <h2 className="text-2xl font-bold text-center mb-6 text-gray-800">
           {isLogin ? "Login" : "Sign Up"}
@@ -89,11 +109,7 @@ function Auth() {
               placeholder="Enter your password"
             />
           </div>
-          {signingError ? (
-            <span className="text-red-500">{errorValue}</span>
-          ) : (
-            ""
-          )}
+          {signingError && <span className="text-red-500">{errorValue}</span>}
           <button
             type="submit"
             className="w-full bg-blue-500 text-white p-3 rounded-lg font-medium hover:bg-blue-600 transition duration-300 flex justify-center items-center"
